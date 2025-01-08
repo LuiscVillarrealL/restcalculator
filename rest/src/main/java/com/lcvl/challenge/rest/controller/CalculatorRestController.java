e5e18886e6a30e22ed06c4d6cb13183b0fd7a495
LuiscVillarrealL <luiscvillarreall@gmail.com>
2025-01-08 15:28:09 +0000

    swagger basic configs

--- rest/src/main/java/com/lcvl/challenge/rest/controller/CalculatorRestController.java
+++ rest/src/main/java/com/lcvl/challenge/rest/controller/CalculatorRestController.java
@@ -3,7 +3,6 @@ package com.lcvl.challenge.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lcvl.challenge.common.util.OperationEnum;
 import com.lcvl.challenge.rest.dto.ResultDto;
 import com.lcvl.challenge.rest.util.CalculationServiceHelper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,6 +24,10 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/calculator")
 @Slf4j
+@Tag(name = "Calculator", description = "Endpoints for arithmetic operations")
+@ApiResponses({ @ApiResponse(responseCode = "400", description = "Invalid request"),
+    @ApiResponse(responseCode = "500", description = "Internal server error"),
+    @ApiResponse(responseCode = "200", description = "Calculation successful") })
 public class CalculatorRestController {
 
   /** The calculation service helper. */
@@ -35,13 +42,13 @@ public class CalculatorRestController {
    * @param requestId the request id
    * @return the sum
    */
+  @Operation(summary = "Add two numbers", description = "Returns the sum of num1 and num2.")
   @GetMapping(value = "/sum")
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<ResultDto> getSum(@RequestParam char[] a, @RequestParam char[] b,
       @RequestHeader(value = "Request-ID", required = false) String requestId) {
 
-    return calculationServiceHelper.handleCalculationRequest(a, b, requestId,
-        OperationEnum.SUM);
+    return calculationServiceHelper.handleCalculationRequest(a, b, requestId, OperationEnum.SUM);
 
   }
 
@@ -53,10 +60,12 @@ public class CalculatorRestController {
    * @param requestId the request id
    * @return the substraction
    */
+  @Operation(
+      summary = "Substract two numbers",
+      description = "Returns the substractions of num1 and num2.")
   @GetMapping(value = "/sub")
   @ResponseStatus(HttpStatus.OK)
-  public ResponseEntity<ResultDto> getSubstraction(@RequestParam char[] a,
-      @RequestParam char[] b,
+  public ResponseEntity<ResultDto> getSubstraction(@RequestParam char[] a, @RequestParam char[] b,
       @RequestHeader(value = "Request-ID", required = false) String requestId) {
 
     return calculationServiceHelper.handleCalculationRequest(a, b, requestId,
@@ -71,10 +80,12 @@ public class CalculatorRestController {
    * @param requestId the request id
    * @return the multiplication
    */
+  @Operation(
+      summary = "Multiply two numbers",
+      description = "Returns the multiplication of num1 and num2.")
   @GetMapping(value = "/multi")
   @ResponseStatus(HttpStatus.OK)
-  public ResponseEntity<ResultDto> getMultiplication(@RequestParam char[] a,
-      @RequestParam char[] b,
+  public ResponseEntity<ResultDto> getMultiplication(@RequestParam char[] a, @RequestParam char[] b,
       @RequestHeader(value = "Request-ID", required = false) String requestId) {
 
     return calculationServiceHelper.handleCalculationRequest(a, b, requestId,
@@ -89,13 +100,13 @@ public class CalculatorRestController {
    * @param requestId the request id
    * @return the division
    */
+  @Operation(summary = "Divide two numbers", description = "Returns the division of num1 and num2.")
   @GetMapping(value = "/div")
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<ResultDto> getDivision(@RequestParam char[] a, @RequestParam char[] b,
       @RequestHeader(value = "Request-ID", required = false) String requestId) {
 
-    return calculationServiceHelper.handleCalculationRequest(a, b, requestId,
-        OperationEnum.DIVIDE);
+    return calculationServiceHelper.handleCalculationRequest(a, b, requestId, OperationEnum.DIVIDE);
   }
 
 }
