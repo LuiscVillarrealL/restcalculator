b33b8de561ef84aea497568652c8f1477259db50
LuiscVillarrealL <luiscvillarreall@gmail.com>
2025-01-12 22:53:06 +0000

    add error for null parameter

--- rest/src/main/java/com/lcvl/challenge/rest/exceptions/GlobalExceptionHandler.java
+++ rest/src/main/java/com/lcvl/challenge/rest/exceptions/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.lcvl.challenge.rest.exceptions;
 import java.math.BigDecimal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import com.lcvl.challenge.rest.dto.ResultDto;
+import jakarta.validation.ConstraintViolationException;
 
 /**
  * The Class GlobalExceptionHandler.
@@ -31,7 +33,7 @@ public class GlobalExceptionHandler {
       message = "Invalid number format for parameter";
     } else {
       // Default handling for other mismatched arguments
-      message =  "Invalid number format for parameter";
+      message = "Invalid number format for parameter";
     }
 
     ResultDto resultDto = new ResultDto(message);
@@ -60,5 +62,36 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ResultDto> handleGenericException(Exception ex) {
     ResultDto errorResponse = new ResultDto("An error occurred: " + ex.getMessage());
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
+
+  /**
+   * Handle missing param.
+   *
+   * @param ex the ex
+   * @return the response entity
+   */
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ResultDto> handleMissingParam(MissingServletRequestParameterException ex) {
+
+    ResultDto errorResponse = new ResultDto(
+        "Missing required parameter '" + ex.getParameterName() + "'");
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+  }
+
+  /**
+   * Handle constraint violation.
+   *
+   * @param ex the ex
+   * @return the response entity
+   */
+  @ExceptionHandler(ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ResultDto> handleConstraintViolation(ConstraintViolationException ex) {
+
+    ResultDto errorResponse = new ResultDto("Validation error: " + ex.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
   }
 }
